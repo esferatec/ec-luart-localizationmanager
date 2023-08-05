3864112d5ede60ebbc92bd96c2f4c0a432985d86
esferatec <136970939+esferatec@users.noreply.github.com>
2023-08-05 07:41:09 +0200

    Initial commit

--- ecluart/lm.lua
+++ ecluart/lm.lua
@@ -1,11 +1,26 @@
 -- Defines a localization management module.
 local lm = {}
 
+lm.customtypes = {}
+
 -- Checks if the parameter is a valid child widget.
 -- isValidChild(parameter: any) -> boolean
 local function isValidChild(parameter)
   local childType = type(parameter)
-  local validTypes = { "Label", "Button", "Checkbox", "Radiobutton", "Groupbox", "MenuItem", "TabItem" }
+  local validTypes = {
+    "Edit",
+    "Entry",
+    "Label",
+    "Button",
+    "Checkbox",
+    "Radiobutton",
+    "Groupbox" }
+
+  if type(lm.customtypes) == "table" then
+    for _, value in ipairs(lm.customtypes) do
+      table.insert(validTypes, tostring(value))
+    end
+  end
 
   for _, validType in ipairs(validTypes) do
     if string.find(childType, validType) then return true end
@@ -34,19 +49,6 @@ function LocalizationManager:constructor(dictionary, language)
   local _dictionary = {}
   local _language = ""
 
-  function self:set_language(value)
-    if not isString(value) then
-      _language = _language ~= "" and _language or os.setlocale("")
-      return
-    end
-
-    _language = value ~= "" and value or os.setlocale("")
-  end
-
-  function self:get_language()
-    return _language
-  end
-
   function self:set_dictionary(value)
     if not isTable(value) then
       _dictionary = next(_dictionary) == nil and {} or _dictionary
@@ -60,40 +62,47 @@ function LocalizationManager:constructor(dictionary, language)
     return _dictionary
   end
 
+  function self:set_language(value)
+    if not isString(value) then
+      _language = _language ~= "" and _language or os.setlocale("")
+      return
+    end
+
+    _language = value ~= "" and value or os.setlocale("")
+  end
+
+  function self:get_language()
+    return _language
+  end
+
   self.language = language
   self.dictionary = dictionary
-
-  self._children = {}
+  self.children = {}
 end
 
 -- Adds a widget and localization key.
 -- add(widget: object, key: string) -> none
-function LocalizationManager:add(widget, key)
-  if not isValidChild(widget) then
-    return
-  end
-
-  if not isString(key) then
-    return
-  end
+function LocalizationManager:add(widget, property, key)
+  if not isValidChild(widget) then return end
+  if not isString(property) then return end
+  if not isString(key) then return end
 
   local newChild = {}
   newChild.widget = widget
+  newChild.property = property
   newChild.key = key
 
-  table.insert(self._children, newChild)
+  table.insert(self.children, newChild)
 end
 
 -- Sets the translated text for each widget.
 -- apply() -> none
 function LocalizationManager:apply()
-  if next(self.dictionary) == nil then
-    return
-  end
+  if next(self.dictionary) == nil then return end
 
-  for _, child in ipairs(self._children) do
+  for _, child in ipairs(self.children) do
     local translatedText = self.dictionary[child.key]
-    if translatedText then child.widget.text = translatedText end
+    if translatedText then child.widget[child.property] = translatedText end
   end
 end
 
